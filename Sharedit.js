fe55aa3daf05384c0e755ded074c614f486b99dc
Thorin Butler <thorin.butler@gmail.com>
2016-05-31 14:13:36 -0500

    added some to the js file and main.html

--- Sharedit.js
+++ Sharedit.js
@@ -1,3 +1,24 @@
 this.Documents = new Mongo.Collection("documents");
 
+if (Meteor.isClient){
+  Template.editor.helpers({
+    docid:function(){
+      var doc = Documents.findOne();
+      if (doc){
+        return doc._id;
+      }
+      else{
+        return undefined;
+      }
+    }
+  })
+}
 
+if (Meteor.isServer){
+  Meteor.startup(function(){
+    //Startup code that creates a document in case there isn't one yet.
+    if (!Documents.findOne()){ // No documents yet!
+      Documents.insert({title: "my new document"});
+    }
+  });
+}
