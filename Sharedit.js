e159c137244e6f6777713b7972abc152ef9882ce
Thorin Butler <thorin.butler@gmail.com>
2016-05-31 14:47:21 -0500

    finalized versions of js and html

--- Sharedit.js
+++ Sharedit.js
@@ -1,24 +1,32 @@
 this.Documents = new Mongo.Collection("documents");
-
-if (Meteor.isClient){
+if (Meteor.isClient) {
+// find the first document in the Documents collection and send back its id
   Template.editor.helpers({
     docid:function(){
       var doc = Documents.findOne();
       if (doc){
         return doc._id;
       }
-      else{
+      else {
         return undefined;
       }
-    }
+    }, 
+    //template helper necessary for passing in our editor data.
+    config:function(){
+      return function(editor){
+        editor.setOption("lineNumbers", true);
+        editor.on("change", function(cm_editor, info){
+          $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+        });        
+      }
+    }, 
   });
 }
-
-if (Meteor.isServer){
-  Meteor.startup(function(){
-    //Startup code that creates a document in case there isn't one yet.
-    if (!Documents.findOne()){ // No documents yet!
-      Documents.insert({title: "my new document"});
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code that runs at startup and creates a document in case it doesn’t exist. 
+    if (!Documents.findOne()){// no documents yet!
+      Documents.insert({title:"my new document"});
     }
   });
 }
